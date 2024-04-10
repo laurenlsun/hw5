bee1acf5a79927f0b40c9b475f2bd3e1f5fa934a
Lauren Sun <sunlaure@usc.edu>
2024-04-10 14:45:14 -0700

    modified wordlehelper

--- wordle.cpp
+++ wordle.cpp
@@ -16,8 +16,9 @@ using namespace std;
 // Add prototypes of helper functions here
 void wordleHelper(std::set<std::string>& words, 
     std::string& input,
-    const std::string& floating,
-    const std::set<std::string>& dict);
+    const std::string floating,
+    const std::set<std::string>& dict,
+    int count);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -30,7 +31,16 @@ std::set<std::string> wordle(
     // make new set
     std::set<std::string> words;
     std::string input = in; // make editable copy that's not const
-    wordleHelper(words, input, floating, dict);
+
+    int count = 0;
+
+    for(int i=0; i<in.length(); i++) {
+        if (in[i]=='-')
+            count++;
+    }
+
+    std::string floatingcopy = floating;
+    wordleHelper(words, input, floatingcopy, dict, count);
 
     return words;
 }
@@ -40,24 +50,26 @@ std::set<std::string> wordle(
 void wordleHelper(
     std::set<std::string>& words, // set to which to insert
     std::string& in, // current state of guessed word
-    const std::string& floating, // letters needed
-    const std::set<std::string>& dict
+    std::string floating, // letters needed
+    const std::set<std::string>& dict,
+    int count
 )
 {
     // base case: filled all the blanks. now check if valid
-    if (in.find("-")==string::npos) { // no - left in the word
+    if (count==0) { // no - left in the word
 
         // assume all floating letters are in our guess
-        bool floatingOK = true;
-        // loop through floating letters
-        for (int i=0; i< floating.length(); i++) {
-            // if can't find one of the letters
-            if (in.find(floating[i])==string::npos)
-                floatingOK = false; // not ok
-        }
+        // FLOATING CHECK
+        // bool floatingOK = true;
+        // // loop through floating letters
+        // for (int i=0; i< floating.length(); i++) {
+        //     // if can't find one of the letters
+        //     if (in.find(floating[i])==string::npos)
+        //         floatingOK = false; // not ok
+        // }
 
         // word in dict AND uses all floating letters
-        if ((dict.find(in) != dict.end()) && (floatingOK)) // word exists in dictionary
+        if (dict.find(in) != dict.end()) // word exists in dictionary
             words.insert(in); // add it
         return;
     }
@@ -65,12 +77,27 @@ void wordleHelper(
     // else need to fill another blank in
     int curr = in.find("-"); // know there's at least 1 - left
 
+    // need to start trying the floating letters
+    if (count<=floating.length()) {
+        for (int i=0; i< floating.length(); i++) {
+            // sub this letter
+            in[curr] = floating[i];
+            // delete floating[i] from floating
+            floating = floating.substr(0, i-1) + floating.substr(i+1, floating.length()-i-1);
+            wordleHelper(words, in, floating, dict, count-1);
+        }
+    }
+    // else more blanks than floating letters
     // try all the letters
     string alphabet = "abcdefghijklmnopqrstuvwxyz";
     for (int i=0; i< alphabet.length(); i++) {
         // sub this letter
         in[curr] = alphabet[i];
-        wordleHelper(words, in, floating, dict);
+
+        // if it was in the floating string remove it from the string
+        int floatingindex = floating.find(alphabet[i]);
+        floating = floating.substr(0, floatingindex-1) + floating.substr(floatingindex+1, floating.length()-floatingindex-1);
+        wordleHelper(words, in, floating, dict, count-1);
     }
 
     // tried all letters and added all the combos that worked, so reset to -

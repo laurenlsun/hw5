90c2260ada2f5f42fe91d8088b1294a4f9a81dc1
Lauren Sun <sunlaure@usc.edu>
2024-04-09 21:13:49 -0700

    initial code for schedwork

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,9 +21,20 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool schedHelp(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    int r,
+    int c);
 
+bool schedOK(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched);
 
 // Add your implementation of schedule() and other helper functions here
+
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -38,8 +49,55 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    vector<int> numShifts; // keep track of total shifts worked by nurses
+    for (int i=0; i<avail[0].size(); i++)
+        numShifts.push_back(0);
 
+    schedHelp(avail, dailyNeed, maxShifts, sched, numShifts, 0, 0);
+}
 
+bool schedHelp(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<int>& numShifts,
+    int r,
+    int c)
+{
+    // base cases: row or col incremented out of range
+    if (r==sched.size()) // we skipped past the last row; everything worked
+        return true;
+    if (c==sched[0].size()) // skipped past the last col; need new row
+        return schedHelp(avail, dailyNeed, maxShifts, sched, r+1, 0);
+
+    // iterate through possibilities
+    for (int i=0; i<avail[0].size(); i++) {
+        sched[r][c] = i; // try a worker
+        // increment their number of shifts worked
+        numShifts[i]++;
+
+        // check if that worked
+        if (schedOK(numShifts, maxShifts)) {
+            // that did work
+            if (schedHelp(avail, dailyNeed, maxShifts, sched, r, c+1))
+                // and everything else down the line worked
+                return true;
+        }
+        // else schedOK was false. let for loop try something else
+        else
+            numShifts[i]--;
+    } 
+    // reset this b/c it didn't work
+    sched[r][c] = INVALID_ID;
+    return false;
 
 }
 
+bool schedOK(vector<int> numShifts, const size_t maxShifts) {
+    for (int i=0; i<numShifts.size(); i++) {
+        if (numShifts[i] > maxShifts)
+            return false;
+    }
+
+    return true;
+}

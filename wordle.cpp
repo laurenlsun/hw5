8ddab5447e2196f045996a55da472af4e08e32e3
Lauren Sun <sunlaure@usc.edu>
2024-04-11 00:57:44 +0000

    debugged infinite loop

--- wordle.cpp
+++ wordle.cpp
@@ -57,49 +57,53 @@ void wordleHelper(
 {
     // base case: filled all the blanks. now check if valid
     if (count==0) { // no - left in the word
-
-        // assume all floating letters are in our guess
-        // FLOATING CHECK
-        // bool floatingOK = true;
-        // // loop through floating letters
-        // for (int i=0; i< floating.length(); i++) {
-        //     // if can't find one of the letters
-        //     if (in.find(floating[i])==string::npos)
-        //         floatingOK = false; // not ok
-        // }
-
-        // word in dict AND uses all floating letters
-        if (dict.find(in) != dict.end()) // word exists in dictionary
+        // cout << "base case: all blanks filled" << endl;
+        
+        if (dict.find(in) != dict.end()) {// word exists in dictionary
+            // cout << in << " added" << endl;
             words.insert(in); // add it
+        }
         return;
     }
+    else {
+        // else need to fill another blank in
+        // cout << "there are " << count << "blanks left" << endl;
+        int curr = in.find("-"); // know there's at least 1 - left
 
-    // else need to fill another blank in
-    int curr = in.find("-"); // know there's at least 1 - left
-
-    // need to start trying the floating letters
-    if (count<=floating.length()) {
-        for (int i=0; i< floating.length(); i++) {
-            // sub this letter
-            in[curr] = floating[i];
-            // delete floating[i] from floating
-            floating = floating.substr(0, i-1) + floating.substr(i+1, floating.length()-i-1);
-            wordleHelper(words, in, floating, dict, count-1);
+        // need to start trying the floating letters
+        if (count<=floating.length()) {
+            // cout << "there are " << floating.length() << " floating letters left to use" << endl;
+            // cout << "trying floating letters " << endl;
+            for (int i=0; i< floating.length(); i++) {
+                // sub this letter
+                in[curr] = floating[i];
+                // delete floating[i] from floating
+                string floatingcopy = floating;
+                floatingcopy.erase(i, 1);
+                // cout << "calling wordleHelper with in=" << in << ", floating=" << floatingcopy << ", count=" << count-1 << endl;
+                wordleHelper(words, in, floatingcopy, dict, count-1);
+            }
         }
-    }
-    // else more blanks than floating letters
-    // try all the letters
-    string alphabet = "abcdefghijklmnopqrstuvwxyz";
-    for (int i=0; i< alphabet.length(); i++) {
-        // sub this letter
-        in[curr] = alphabet[i];
+        else {
+            // else more blanks than floating letters
+            // try all the letters
+            // cout << "trying alphabet" << endl;
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            for (int i=0; i< alphabet.length(); i++) {
+                // sub this letter
+                in[curr] = alphabet[i];
+                // if it was in the floating string remove it from the string
+                string floatingcopy = floating;
+                if (floating.find(alphabet[i])!=string::npos) { // we tried a letter from the floating string
+                    floatingcopy.erase(floating.find(alphabet[i]),1);
+                }
+                // cout << "calling wordleHelper with in=" << in << ", floating=" << floating << ", count=" << count-1 << endl;
+                wordleHelper(words, in, floatingcopy, dict, count-1);
+            }
+        }
 
-        // if it was in the floating string remove it from the string
-        int floatingindex = floating.find(alphabet[i]);
-        floating = floating.substr(0, floatingindex-1) + floating.substr(floatingindex+1, floating.length()-floatingindex-1);
-        wordleHelper(words, in, floating, dict, count-1);
+        // tried all letters and added all the combos that worked, so reset to -
+        // cout << "all combos failed" << endl;
+        in[curr] = '-';
     }
-
-    // tried all letters and added all the combos that worked, so reset to -
-    in[curr] = '-';
 }
